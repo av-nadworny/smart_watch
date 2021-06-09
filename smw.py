a4bd5e1f6182510fab6786b1dc24d6affae5e2c9
av-nadworny <a.v.nadworny@gmail.com>
2021-06-09 19:11:07 +0300

    add saving SMART to json file

--- smw.py
+++ smw.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 SNAPSHOT_FILENAME = "snapshot.json"
 CONFIG_FILENAME = "config.json"
-DEFAULT_CONFIG = {'devices': {'*'}, 'attr_ids': {'*'}}
+DEFAULT_CONFIG = {'dev_filter': {'*'}, 'attr_filter': {'*'}}
 
 
 def load_json(filename):
@@ -26,29 +26,28 @@ def save_json(snapshot, filename):
 def parse_attr_string(attr):
     result = {}
     result['id'] = attr[:3].strip()
+    result['name'] = attr[4:27].strip()
     result['raw'] = attr[69:].strip()
     return result
 
 
-def get_device_smart(device, attr_ids):
+def get_device_smart(device):
     smart = {}
     for attr in device.attributes:
         # if attr is not empty...
         if attr:
             # get values from string
             parsed_attr = parse_attr_string(str(attr))
-            attr_id, attr_raw = parsed_attr['id'], parsed_attr['raw']
-            # add attribute accordingly to "attributes" set
-            if "*" in attr_ids or attr_id in attr_ids:
-                smart[attr_id] = attr_raw
+            attr_id = parsed_attr['id']
+            attr_name = parsed_attr['name']
+            attr_raw = parsed_attr['raw']
+            smart[attr_id] = {'name': attr_name, 'raw': attr_raw}
     return smart
 
 
-def make_snapshot(config=DEFAULT_CONFIG):
-    devices = config['devices']
-    attr_ids = config['attr_ids']
+def make_snapshot():
     # get storage devices' mountpoints from smartctl output
-    # (pySMART DeviceList not working)
+    # (pySMART DeviceList is empty)
     mp_strings = subprocess.run('smartctl.exe --scan', capture_output=True) \
         .stdout.decode('utf-8').split('\r\n')
     # get ATA devices only
@@ -58,14 +57,12 @@ def make_snapshot(config=DEFAULT_CONFIG):
             mp_list.append(string.split(' ')[0])
     dev_list = []
     for mountpoint in mp_list:
-        # add devices accordingly to "devices" set
-        if "*" in devices or mountpoint in devices:
-            dev_list.append(Device(mountpoint))
+        dev_list.append(Device(mountpoint))
 
     smarts = {}
     for device in dev_list:
         device_name = f'{device.model} :: {device.serial}'
-        device_smart = get_device_smart(device, attr_ids)
+        device_smart = get_device_smart(device)
         smarts[device_name] = device_smart
 
     snapshot = {}
@@ -87,8 +84,8 @@ if __name__ == "__main__":
     # load config
     config = load_json(CONFIG_FILENAME)
     # make snapshot
-    config = {'devices': {'*'}, 'attr_ids': {'5', '196', '197'}}
-    new_snapshot = make_snapshot(config)
+    # config = {'dev_filter': {'*'}, 'attr_filter': {'*'}}
+    new_snapshot = make_snapshot()
     print(new_snapshot)
     # load last snapshot
     last_snapshot = load_json(SNAPSHOT_FILENAME)
@@ -101,6 +98,6 @@ if __name__ == "__main__":
             print("Differences found!")
             print_report(difference)
             # waiting for user action
+            save_json(new_snapshot, SNAPSHOT_FILENAME)
             input("Press Enter to exit...")
     # save new snapshot
-    # save_json(new_snapshot, SNAPSHOT_FILENAME)
